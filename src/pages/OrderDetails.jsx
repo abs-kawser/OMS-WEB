7295877ac60bbbe6fe8be5ac8986bbfd67993c4a
abs-kawser <abskawser77000@gmail.com>
2024-01-02 17:54:23 +0600

    order details start

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const OrderDetails = () => {
+
+
   return (
+
     <div>OrderDetails</div>
+    
   )
 }
 
 export default OrderDetails
+
+
+
+//  first  need two button 
+//fetch product data with a qty box 
+//those selected qty box was show in orderDetails 
+//orderDetails have submit button 
